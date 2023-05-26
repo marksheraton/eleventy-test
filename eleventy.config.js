9ac518dd58745a6672728f4a1f8a4aeb8c366c16
Mark Sheraton <marksheraton@users.noreply.github.com>
2023-05-26 15:17:33 -0700

    initial commit

---
+++ eleventy.config.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+	return {
+		
+		markdownTemplateEngine: "njk",
+		dataTemplateEngine: 'njk',
+		htmlTemplateEngine: "njk",
+		
+		dir: {
+			input: "src",
+			output: "_site"
+		}
+	};
+};
