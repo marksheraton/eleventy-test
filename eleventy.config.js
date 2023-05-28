0e4e0e5d6b893add39450bff631610e2485bcaa3
Mark Sheraton <marksheraton@users.noreply.github.com>
2023-05-27 21:12:22 -0700

    Work on blog and dates

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,11 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
+	
+	eleventyConfig.addFilter("postDate", (dateObj) => {
+		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+	});
+	
 	return {
 		
 		markdownTemplateEngine: "njk",
