7706634667d43bebfd200125d3012e0310a7887c
Mark Sheraton <marksheraton@users.noreply.github.com>
2023-05-29 23:23:05 -0700

    Design updates

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,11 @@
 const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
-		
+	
+	eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+	
 	eleventyConfig.addFilter("postDate", (dateObj) => {
-		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
 	});
 	
 	return {
