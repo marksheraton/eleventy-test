ba912dbfb9d52871f7b88c7537673f8f27af6dc5
Mark Sheraton <marksheraton@users.noreply.github.com>
2023-06-02 16:32:45 -0700

    Add people to company page and add image passthrough to 11ty

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,11 +2,16 @@ const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
 	
+	// Current year for footer
 	eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 	
+	// Make dates human readable
 	eleventyConfig.addFilter("postDate", (dateObj) => {
 		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
 	});
+	
+	// Copy `assets/images/` to `_site/assets/images`
+	eleventyConfig.addPassthroughCopy("src/assets/images");
 	
 	return {
 		
